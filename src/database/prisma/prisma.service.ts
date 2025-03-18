2da927d844448dbbbf754b4298d48f6ef2d52a99
Gabriel Viana <gabrielguilherme13@hotmail.com>
2025-03-18 08:43:10 -0300

    feat: prisma configuration

---
+++ src/database/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async enableShutdownHooks(app: INestApplication) {}
+}
