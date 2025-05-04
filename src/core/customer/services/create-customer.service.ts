701bb8f1fe00de63ae99927aba0936f75ca6ebe4
Gabriel <gabrielcp4@outlook.com>
2025-05-04 17:48:34 -0300

    :sparkles:feat: create-customer.service now checks for duplicated rg value before inserting refacotr: whole service code refactored to uncle bob paradigms

--- src/core/customer/services/create-customer.service.ts
+++ src/core/customer/services/create-customer.service.ts
@@ -7,16 +7,42 @@ export class CreateCustomerService {
   constructor(private readonly customerRepository: CustomerRepository) {}
 
   async execute(data: CreateCustomerInput) {
-    const customerExists = await this.customerRepository.findByCpfCnpj(
-      data.cpfCnpj,
-    );
-
-    if (customerExists) {
-      throw new ConflictException('Customer already exists');
-    }
+    await this.checkForDuplicatedUniqueValues(data.cpfCnpj, data.rg);
 
     const customer = await this.customerRepository.create(data);
 
     return customer;
+  }
+
+  private async checkForDuplicatedUniqueValues(
+    cpfCnpj: string,
+    rg: string | undefined,
+  ) {
+    await this.checkIfCostumerExistsByCpfCnpj(cpfCnpj);
+    await this.checkForDuplicateRg(rg);
+  }
+  
+  private async checkIfCostumerExistsByCpfCnpj(cpfCnpj: string) {
+    const costumerExists = await this.customerRepository.findByCpfCnpj(cpfCnpj);
+
+    if (costumerExists) throw new ConflictException('Costumer already exists');
+  }
+
+  private async checkForDuplicateRg(rg: string | undefined) {
+    const isRgValid = this.checkIfRgIsValid(rg);
+
+    if (isRgValid) {
+      await this.checkIfCostumerExistsByRg(rg ?? '');
+    }
+  }
+
+  private checkIfRgIsValid(rg: string | undefined): boolean {
+    return rg ? true : false;
+  }
+
+  private async checkIfCostumerExistsByRg(rg: string) {
+    const costumerExists = await this.customerRepository.findByRg(rg);
+
+    if (costumerExists) throw new ConflictException('Costumer already exists');
   }
 }
